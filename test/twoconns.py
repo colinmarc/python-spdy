f2659021ef4033f251c9f1fedb5ce7c4b6f12b99
Colin Marc <colinmarc@gmail.com>
2012-03-22 15:52:05 -0400

    updated two conn test

--- test/twoconns.py
+++ test/twoconns.py
@@ -3,10 +3,18 @@ from spdy.connection import *
 server = Connection(SERVER)
 client = Connection(CLIENT)
 
-frame = SynStream(2, client.next_stream_id, {'dood': 'balls', 'stuff': 'otherstuff'})
+frame = SynStream(2, client.next_stream_id, headers={'dood': 'balls', 'stuff': 'otherstuff'})
 client.put_frame(frame)
 chunk = client.outgoing()
 
 server.incoming(chunk)
 frame2 = server.get_frame()
 print(frame2.headers)
+
+frame3 = SynReply(2, server.next_stream_id, headers={'got it': 'yup', 'roger': 'roger'})
+server.put_frame(frame3)
+chunk2 = server.outgoing()
+
+client.incoming(chunk2)
+frame4 = client.get_frame()
+print(frame4.headers)
