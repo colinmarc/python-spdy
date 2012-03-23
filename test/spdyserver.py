223cdcea148da1d009c0790ba2a1c6cbc61a5484
Colin Marc <colinmarc@gmail.com>
2012-03-23 15:17:16 -0400

    hello world!!!

--- test/spdyserver.py
+++ test/spdyserver.py
@@ -1,5 +1,8 @@
 import socket
 import ssl
+import spdy.frames
+from spdy.connection import *
+from pprint import pprint
 
 server = socket.socket()
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -9,20 +12,48 @@ server.listen(5)
 ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 ctx.load_cert_chain('server.crt', 'server.key')
 ctx.set_npn_protocols(['spdy/2'])
-b = bytearray()
+
+def handle_frame(conn, f):
+	print("server says,", f)
+		
+	if isinstance(f, spdy.frames.Ping):
+		ping = spdy.frames.Ping(f.uniq_id)
+		conn.put_frame(ping)	
+		print(ping, "says client")
+	elif isinstance(f, spdy.frames.SynStream):
+		pprint(f.headers)	
+		resp = spdy.frames.SynReply(f.stream_id, {'status': '200 OK', 'version': 'HTTP/1.1'}, flags=0)
+		conn.put_frame(resp)
+		print(resp, "says client")
+		data = spdy.frames.DataFrame(f.stream_id, b"hello world!!!", flags=1)
+		conn.put_frame(data)
+		print(data, "says client")
+
 try:
 	while True:
 		try:
 			sock, sockaddr = server.accept()
 			ss = ctx.wrap_socket(sock, server_side=True)
+
+			conn = Connection(SERVER) 
+
+			while True:
+				d = ss.recv(1024)
+				conn.incoming(d)
+				while True:
+					f = conn.get_frame()
+					if not f:
+						break
+					handle_frame(conn, f)
+
+				outgoing = conn.outgoing()
+				if outgoing: 
+					ss.sendall(outgoing)
+
 		except Exception as exc:
 			print(exc)
+			if not "EOF" in str(exc): raise
 			continue
 
-		while True:
-			d = ss.recv(1024)
-			b.extend(d)
-			print(d)
 finally:
 	server.close()
-	print(b)
