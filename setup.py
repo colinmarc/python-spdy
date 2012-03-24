2e131f0442b73e7fbf0dd9feebf927441450afc8
Colin Marc <colinmarc@gmail.com>
2012-03-24 13:59:12 -0400

    updated setup.py to include requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ zlib_wrapper = Extension(
 
 setup(
 	name='spdy',
+	version='0.2',
+	description='a parser/muxer/demuxer for spdy frames',
+	author='Colin Marc',
+	author_email='colinmarc@gmail.com',
+	url='http//www.github.com/colinmarc/python-spdy',
+	requires=['Cython (>=0.15.1)', 'bitarray (>=0.7.0)'],
 	cmdclass={'build_ext': build_ext},
 	ext_modules=[zlib_wrapper],
 	packages=['spdy'],
